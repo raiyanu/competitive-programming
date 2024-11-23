0f0ed74ad9d086a4af2a436da3301b1da73bb5aa
--global <ray38250968@gmail.com>
2024-11-24 01:03:43 +0530

    progressing... in anagram problem

--- leetcode-IsAnagram.java
+++ leetcode-IsAnagram.java
@@ -8,6 +8,7 @@ class Main {
         else System.out.println("Test " + (++i)+ ": \u001B[31mFailed\u001B[0m");
     }
     public static void main(String[] args) {
+        System.out.println(('c'-'a'));
         test("anagram", "nagaram", true);
         test("rat", "car", false);
         test("anagram", "nagaram", true);
@@ -17,15 +18,26 @@ class Main {
 
 
 class Solution {
-    HashMap<Character, Integer> Smap = new HashMap<>();
-    HashMap<Character, Integer> Tmap = new HashMap<>();
+    HashMap<Integer, Integer> Smap = new HashMap<>();
+    HashMap<Integer, Integer> Tmap = new HashMap<>();
 
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        for (Character ch : s.toCharArray()) Smap.put(ch, Smap.getOrDefault(ch, 0) + 1);
-        for (Character ch : t.toCharArray()) Tmap.put(ch, Tmap.getOrDefault(ch, 0) + 1);
-        for (char key : Smap.keySet()) if (Tmap.get(key) != Smap.get(key)) return false;
-        for (char key : Tmap.keySet()) if (Tmap.get(key) != Smap.get(key)) return false;
+        if (s.length() != t.length())
+            return false;
+        fill(s, Smap);
+        fill(t, Tmap);
+        for (Integer key : Smap.keySet())
+            if (Tmap.get(key) != Smap.get(key))
+                return false;
+        for (Integer key : Tmap.keySet())
+            if (Tmap.get(key) != Smap.get(key))
+                return false;
         return true;
+    }
+
+    private void fill(String s, HashMap<Integer, Integer> map) {
+        for (Character ch : s.toCharArray())
+            if (ch >= 'a' && ch <= 'z')
+                map.put(ch - 'a', map.getOrDefault(ch - 'a', 0) + 1);
     }
 }
