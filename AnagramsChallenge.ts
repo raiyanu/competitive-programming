017ac478db0213f234074b3ca7b8b6e1363b479f
Raiyan <raiyan.c.me@gmail.com>
2025-10-22 17:41:44 +0530

    optimized Anagram function for efficiency

--- AnagramsChallenge.ts
+++ AnagramsChallenge.ts
@@ -1,11 +1,12 @@
 function isAnagrams(s: string, t: string) {
-    const sMap: Record<string, number> = {},
-        tMap: Record<string, number> = {};
+    if (s.length !== t.length) return false;
+    const sMap: Record<string, number> = {};
     for (let i: number = 0; i < s.length; i++) sMap[s[i]] = (sMap[s[i]] || 0) + 1;
-    for (let i: number = 0; i < t.length; i++) tMap[t[i]] = (tMap[t[i]] || 0) + 1;
-    if (Object.keys(sMap).length !== Object.keys(tMap).length) return false;
-    for (const key of Object.keys(sMap))
-        if (sMap[key] !== tMap[key]) return false;
+    for (let i: number = 0; i < t.length; i++) {
+        const key = t[i];
+        if (!sMap[key]) return false;
+        else sMap[key]--;
+    }
     return true;
 }
 
