fa04f3a526aa696c62490962c1fcdb1cb879f614
Raiyan <raiyan.c.me@gmail.com>
2025-10-27 19:32:42 +0530

    code ref

--- quickSort.ts
+++ quickSort.ts
@@ -1,13 +1,18 @@
+/**
+ * To Sort Array Using Quick Sort
+ * @param arr Array<Number>
+ * @returns Array<Number>
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n)
+ */
 function quickSort(arr: Array<number>): Array<number> {
     if (arr.length < 2) return arr;
-    let pivot = arr[arr.length - 1], left = [], right = [];
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > pivot) {
-            right.push(arr[i])
-        } else {
-            left.push(arr[i])
-        }
-    }
+    let pivot = arr[arr.length - 1],
+        left = [],
+        right = [];
+    for (let i = 0; i < arr.length - 1; i++)
+        if (arr[i] > pivot) right.push(arr[i]);
+        else left.push(arr[i]);
     return [quickSort(left), pivot, quickSort(right)].flat();
 }
 
@@ -19,4 +24,4 @@ console.log(quickSort([1, 2, 3, 4, 7, -12, -32]));
 console.log(quickSort([55, -55, 1, 2, 3, 4, 7, -12, -32, 213, -123]));
 console.log(quickSort([0, 0]));
 console.log(quickSort([]));
-console.log(quickSort([4, 1, 2, 3,]));
+console.log(quickSort([4, 1, 2, 3]));
